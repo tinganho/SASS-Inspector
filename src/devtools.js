6a08251d32e3eeb8f23e4453fd9058b9bf6da006
Tingan Ho <tingan87@gmail.com>
2014-08-12 14:49:24 +0800

    Fixes GA analytics

--- src/devtools.js
+++ src/devtools.js
@@ -28,4 +28,14 @@
       updateElementProperties();
       chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
     });
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-53743351-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 })();
